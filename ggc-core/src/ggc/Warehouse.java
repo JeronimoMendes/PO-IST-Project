74708365a49884f06924aa73de9478b08576b945
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-10-08 20:22:50 +0100

    First commit

---
+++ ggc-core/src/ggc/Warehouse.java
@@ -0,0 +1,30 @@
+package ggc;
+
+import ggc.exceptions.*;
+import java.io.IOException;
+import java.io.Serializable;
+
+// FIXME import classes (cannot import from pt.tecnico or ggc.app)
+
+/**
+ * Class Warehouse implements a warehouse.
+ */
+public class Warehouse implements Serializable {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 202109192006L;
+
+  // FIXME define attributes
+  // FIXME define contructor(s)
+  // FIXME define methods
+
+  /**
+   * @param txtfile filename to be loaded.
+   * @throws IOException
+   * @throws BadEntryException
+   */
+  void importFile(String txtfile) throws IOException, BadEntryException /* FIXME maybe other exceptions */ {
+    //FIXME implement method
+  }
+
+}
