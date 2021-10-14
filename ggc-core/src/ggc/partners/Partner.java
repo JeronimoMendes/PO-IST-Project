6130c29e8dedf83f56603853aec94bacaa319ebb
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-10-14 16:48:28 +0100

    Create primitive `Partner` class

---
+++ ggc-core/src/ggc/partners/Partner.java
@@ -0,0 +1,33 @@
+package ggc.partners;
+
+import java.io.Serializable;
+
+
+public class Partner implements Serializable {
+	/** Unique id of Partner */
+	private String _id;
+
+	/** Name of Partner */
+	private String _name;
+
+	/** Address of Partner */
+	private String _address;
+
+	/** Transaction score of Partner */
+	private int _score = 0;
+
+	/**
+	 * Partner's main constructor
+	 * 
+	 * @param id unique id
+	 * 
+	 * @param name name
+	 * 
+	 * @param address address 
+	 */
+	public Partner(String id, String name, String address) {
+		super();
+		_id = id;
+		_name = name;
+	}
+}
