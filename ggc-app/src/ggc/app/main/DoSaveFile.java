ce07647541f2cddf9b1acc7c65613beb3f8c5451
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-10-25 18:34:21 +0100

    Refactor prompt

--- ggc-app/src/ggc/app/main/DoSaveFile.java
+++ ggc-app/src/ggc/app/main/DoSaveFile.java
@@ -15,9 +15,6 @@ import java.io.IOException;
 
 class DoSaveFile extends Command<WarehouseManager> {
 
-	/** Form to ask for the filename only if it is empty */
-	Form _fileForm = new Form();
-
 	/** @param receiver */
 	DoSaveFile(WarehouseManager receiver) {
 		super(Label.SAVE, receiver);
@@ -27,7 +24,7 @@ class DoSaveFile extends Command<WarehouseManager> {
 	public final void execute() throws CommandException {
 		try {
 			if (_receiver.getFilename().isEmpty()) {
-				String newFilename = _fileForm.requestString(Prompt.newSaveAs());
+				String newFilename = Form.requestString(Prompt.newSaveAs());
 				_receiver.saveAs(newFilename);
 			} else {
 				_receiver.save();
